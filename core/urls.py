d8bdc55f52b104722fcb6d3840b714b035eecee5
wenyug <khgrant427@gmail.com>
2015-05-14 23:22:44 -0500

    basic blog is working!

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+import core.views as coreviews
+
+urlpatterns = patterns('',
+
+ url(r'^$', coreviews.LandingView.as_view()),
+ url(r'location/$', coreviews.LocationListView.as_view()),
+ url(r'location/(?P<pk>\d+)/detail/$', coreviews.LocationDetailView.as_view(), name="location_list"),
+ )
