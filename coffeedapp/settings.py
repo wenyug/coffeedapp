2d9b40328b462f69165bfc80c5a13c32ef79bd05
wenyug <khgrant427@gmail.com>
2015-05-31 20:15:12 -0500

    try again 1

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -115,7 +115,7 @@ else:
 
             'ENGINE': 'django.db.backends.sqlite3',
 
-            'NAME': os.path.join(MAIN_DIR, 'data.sqlite3'),
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
         }
 
